14aeada7286fcf0ab2af85f353c2a3f0ce67debe
pxn <pu6422534@163.com>
2019-10-26 18:57:52 +0800

    first commit

---
+++ src/main/java/com/xima/per/PerformanceApplication.java
@@ -0,0 +1,14 @@
+package com.xima.per;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PerformanceApplication.class, args);
+        System.out.println("Start End...");
+    }
+
+}
